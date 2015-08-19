566ac3f209e691e321c094bc38863ae3be894ac2
Morgan Plant <morgantheplant@gmail.com>
2015-08-18 18:01:53 -0700

    add basic express server

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+    res.sendfile(__dirname + '/index.html');
+});
+
+
+
+
+var port = process.env.PORT || 3000;
+var server = app.listen(port);
+
+console.log('Example app listening on port ', port);
+
